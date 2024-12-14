9ae814df892fcd8e4a47183a47b7ce1f6c1727f3
Satendra Kumar Parteti <satendrakumarparteti.work@gmail.com>
2024-12-15 01:34:44 +0530

    added comments

--- src/context/DateContext.jsx
+++ src/context/DateContext.jsx
@@ -1,12 +1,16 @@
 import { createContext, useContext, useState } from "react";
 
+// Create a context to hold the date state
 const dateContext = createContext(null);
 
+// Custom hook to consume the date context in other components
 export const useDateContext = () => {
   return useContext(dateContext);
 };
 
+// DateProvider component to provide date context to its children
 export const DateProvider = ({ children }) => {
+  // The date state is initialized to the current date
   const [date, setDate] = useState(new Date());
   return (
     <dateContext.Provider value={{ date, setDate }}>
