b1427b3ea45d05d075620bce5ded17af12c565d0
Satendra Kumar Parteti <satendrakumarparteti.work@gmail.com>
2024-12-15 01:40:39 +0530

    added comments

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import CalenderContainer from "./components/customs/CalenderContainer";
 import { Events } from "./components/customs/Events";
 
 function App() {
+
+  // Just a normal credit
   useEffect(() => {
     console.clear();
     console.log(
@@ -13,14 +15,19 @@ function App() {
 
   return (
     <div className="p-4">
+      {/* Header section */}
       <div className="text-center font-bold md:text-3xl">
         <h1>Event Manager</h1>
         <p className="text-sm font-normal text-black/20">by Satendra</p>
       </div>
+
+      {/* Container for the calendar and event list */}
       <div className="calender-container rounded-lg md:flex-row">
         <div className="calender flex flex-col items-center justify-center p-5">
           <CalenderContainer />
         </div>
+
+        {/* Events list container with a border and padding */}
         <div className="list h-full border p-2">
           <Events />
         </div>
