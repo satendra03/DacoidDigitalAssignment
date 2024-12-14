9959050683e1ea031b262f0f4fbbdb3be0924ad5
Satendra Kumar Parteti <satendrakumarparteti.work@gmail.com>
2024-12-15 00:51:41 +0530

    vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx";
 import { EventProvider } from "./context/EventContext";
 import { Toaster } from "react-hot-toast";
 import { DateProvider } from "./context/DateContext";
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -14,5 +15,6 @@ createRoot(document.getElementById("root")).render(
       </EventProvider>
     </DateProvider>
     <Toaster />
+    <Analytics />
   </StrictMode>
 );
